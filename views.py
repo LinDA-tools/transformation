bd24681c300cfc6c2ddddd12bc399836e47b2276
almey <alanmeyerclp@gmail.com>
2015-03-09 18:28:03 +0100

    added CSV upload

--- views.py
+++ views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
+from .forms import UploadFileForm
 
 
 def csv_view(request):
@@ -9,4 +12,12 @@ def csv_view(request):
 
 
 def index(request):
-    return render(request, 'transformation/index.html', {})
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            for chunk in request.FILES['file'].chunks():
+                print(chunk)
+            return HttpResponseRedirect('')
+    else:
+        form = UploadFileForm()
+    return render_to_response('transformation/index.html', {'form': form}, context_instance=RequestContext(request))
