04db053a66a2c712aada4497712cb19fc18094ae
bbrand84 <benjaminbrand@gmx.de>
2015-04-10 12:42:17 +0200

    renewed urls.py. added 'processing...' information as it may take longto write file into db

--- urls.py
+++ urls.py
@@ -3,9 +3,8 @@ from transformation import views
 from .views import *
 
 urlpatterns = patterns('',
-    url(r'index.html$', views.index, name='transformation-index'),
-    url(r'datachoice', views.data_choice, name='data-choice-view'),
-    url(r'csv/upload', views.csv_upload, name='csv-upload-view'),
-    url(r'csv/columnchoice', views.csv_column_choice, name='csv-column-choice-view'),
+    url(r'^$', views.data_choice, name='data-choice-view'),
+    url(r'csv/step/1', views.csv_upload, name='csv-upload-view'),
+    url(r'csv/step/2', views.csv_column_choice, name='csv-column-choice-view'),
 
 )
