d565b583a5cd4b8c26bc920ae0beb9e1c6aafce5
almey <alanmeyerclp@gmail.com>
2015-07-02 14:40:06 +0200

    added validation for subject URI

--- static/js/common.js
+++ static/js/common.js
@@ -7,7 +7,7 @@ function toLetters(num) {
     return pow ? toLetters(pow) + out : out;
 }
 
-
+var validURL;
 function create_subjects_from_model_sceleton(model) {
 	var sceleton = "";
 	var base_url = "";
@@ -34,12 +34,23 @@ function create_subjects_from_model_sceleton(model) {
 						subjects_array[elem] = sceleton;
 					}
 					subjects_array[elem] = subjects_array[elem].replace(new RegExp("{"+col_name+"}","g"), $(this)[0]['orig_val']);
-				}				
+				}
 			});
 			if(model && model['subject']['blank_nodes'] == "false" || !model['subject']['blank_nodes'])
 				subjects_array[row] = "<" + base_url + subjects_array[row] + ">";
 		}
 	});
+    validURL = true;
+    var testURL = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+    subjects_array.forEach(function(entry) {
+        var result = entry
+        if ((entry.charAt(0) == '<') && (entry.charAt(entry.length - 1) == '>')) {
+            result = entry.substring(1, entry.length - 1);
+        }
+        validURL = validURL && testURL.test(result);
+        console.log(result);
+        console.log(validURL);
+    });
 	return subjects_array;
 }
 
