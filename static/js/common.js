2148922f31f45f04225ee4d7638728a234ef434a
Benjamin Brand <benjaminbrand@gmx.de>
2015-07-09 14:46:21 +0200

    prevented ENTER button for form submit in every step

--- static/js/common.js
+++ static/js/common.js
@@ -27,6 +27,15 @@ lindaGlobals = {
 	}*/
 }
 
+//supress Enter key for form
+function stopRKey(evt) {
+  var evt = (evt) ? evt : ((event) ? event : null);
+  var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+  if ((evt.keyCode == 13) && (node.type=="text"))  {return false;}
+}
+
+document.onkeypress = stopRKey;
+
 
 
 //from http://linda.epu.ntua.gr:8000/api/vocabularies/versions/
